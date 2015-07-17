6833ac5894e60fc64adf168b1dfdc59a1dea2e02
aim4life <abhijitchoudhury@hotmail.com>
2015-07-16 23:22:48 -0500

    Added First Model, iify: Immediately Invoked function.

---
+++ Angular1/script.js
@@ -0,0 +1,14 @@
+// Code goes here
+
+var ask = function(){
+  prompt("what is your name");
+  console.log("Prompted");
+}
+var con = function(){
+  confirm("You sure");
+  console.warn("warning");
+}
+var alt = function(){
+  alert("You sure");
+  console.error("warning");
+}
